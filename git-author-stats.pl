0befde18084801f713b9af5661e229d0be8eda19
Mike Kelly <pioto@pioto.org>
2009-10-14 14:49:29 -0400

    Add title & color.

--- git-author-stats.pl
+++ git-author-stats.pl
@@ -34,10 +34,18 @@ $data[11] = $data[11] / $total * 100;
 
 my $uri = URI->new('http://chart.apis.google.com/chart');
 
+my $repo_url = $repo->config('remote.origin.url') || $repo->repo_path();
+my @branches = $repo->command('branch');
+my ($branch) = grep {/^\*/} @branches;
+$branch =~ s/^\*\s*// if $branch;
+$repo_url .= " on $branch" if $branch && $branch ne 'master';
+
+$uri->query_param('chtt', "Top 10 Authors for $repo_url"); # chart title
 $uri->query_param('chs', '650x250'); # chart size
 $uri->query_param('cht', 'p3'); # chart type
-$uri->query_param('chd', 't:'.join(',', @data)); #
-$uri->query_param('chl', join('|', @labels));
+$uri->query_param('chco', '006600'); # colors
+$uri->query_param('chd', 't:'.join(',', @data)); # data
+$uri->query_param('chl', join('|', @labels)); # labels
 
 print "$uri\n";
 
